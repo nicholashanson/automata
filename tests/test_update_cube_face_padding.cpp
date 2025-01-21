f72f62912ffdf83f8be39a7d3e8e16adb2e56374
Nick <nicholashanson27@outlook.com>
2025-01-21 18:11:54 +0800

    modified test_update_cube_face_padding.cpp

--- tests/test_update_cube_face_padding.cpp
+++ tests/test_update_cube_face_padding.cpp
@@ -1,19 +1,21 @@
 #include <boost/test/unit_test.hpp>
 #include "conway.hpp"
 
+using namespace conway;
+
 BOOST_AUTO_TEST_CASE( UpdateCubeFacePadding )
 {
-    conway::cube<conway::a25, conway::md5x5> cube( conway::cube_face_0_before_update,
-                                                   conway::blank_face,
-                                                   conway::blank_face,
-                                                   conway::blank_face,
-                                                   conway::blank_face,
-                                                   conway::blank_face );
+    cube<a25, md5x5> cube( cube_face_0_before_update,
+                           blank_face,
+                           blank_face,
+                           blank_face,
+                           blank_face,
+                           blank_face );
     cube.update_faces();
-    BOOST_CHECK( cube.is_equal<1>( conway::board<conway::a25, conway::md5x5>( conway::cube_face_1_after_update ) ) );
-    BOOST_CHECK( cube.is_equal<3>( conway::board<conway::a25, conway::md5x5>( conway::cube_face_3_after_update ) ) );
-    BOOST_CHECK( cube.is_equal<4>( conway::board<conway::a25, conway::md5x5>( conway::cube_face_4_after_update ) ) );
-    BOOST_CHECK( cube.is_equal<5>( conway::board<conway::a25, conway::md5x5>( conway::cube_face_5_after_update ) ) );
+    BOOST_CHECK( cube.is_equal<1>( board<a25, md5x5>( cube_face_1_after_update ) ) );
+    BOOST_CHECK( cube.is_equal<3>( board<a25, md5x5>( cube_face_3_after_update ) ) );
+    BOOST_CHECK( cube.is_equal<4>( board<a25, md5x5>( cube_face_4_after_update ) ) );
+    BOOST_CHECK( cube.is_equal<5>( board<a25, md5x5>( cube_face_5_after_update ) ) );
 }
 
 
