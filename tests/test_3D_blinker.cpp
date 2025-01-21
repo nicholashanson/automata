c5fcc2fb6c87e91bf21ae33f583cb96a241815da
Nick <nicholashanson27@outlook.com>
2025-01-21 21:22:58 +0800

    changed test name

--- tests/test_3D_blinker.cpp
+++ tests/test_3D_blinker.cpp
@@ -3,13 +3,13 @@
 
 using namespace conway;
 
-BOOST_AUTO_TEST_CASE( 3DBlinkerTest )
+BOOST_AUTO_TEST_CASE( Blinker3DTest )
 {
-    toroidal_cube<board<a49, md7x7>> tc( blank_a49,
-                                         blank_a49,
-                                         blinker_period_0,
-                                         blank_a49
-                                         blank_a49 );
-    tc.evolve();
-    BOOST_CHECK( ( tc.get_plane<0,0> == board<a49, md7x7>( blinker_period_1 ) ) );
+    toroidal_cube<a49, md7x7, board, 5> tc( blank_a49,
+                                            blank_a49,
+                                            blinker_period_0,
+                                            blank_a49,
+                                            blank_a49 );
+    tc.evolve_cube();
+    BOOST_CHECK( tc.equal_boards<0>( 3, board<a49, md7x7>( blank_a49 ) ) );
 }
