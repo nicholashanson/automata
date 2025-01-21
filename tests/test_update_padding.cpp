203e63c7a7e5f5c26e51c81ad4e45dc42262d0f8
Nick <nicholashanson27@outlook.com>
2025-01-21 21:25:20 +0800

    cleaned up with using namespace

--- tests/test_update_padding.cpp
+++ tests/test_update_padding.cpp
@@ -1,9 +1,11 @@
 #include <boost/test/unit_test.hpp>
 #include "conway.hpp"
 
+using namespace conway;
+
 BOOST_AUTO_TEST_CASE( UpdatePaddingTest )
 {
-    conway::board<conway::a49, conway::md7x7> bd( conway::board_update_before );
-    conway::update_padding( bd );
-    BOOST_CHECK( ( bd == conway::board<conway::a49, conway::md7x7>( conway::board_update_after ) ) );
+    board<a49, md7x7> bd( board_update_before );
+    update_padding( bd );
+    BOOST_CHECK( ( bd == board<a49, md7x7>( board_update_after ) ) );
 }
