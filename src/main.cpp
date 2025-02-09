9c2256dace9386b8879650869288ed0655003892
Nick <nicholashanson27@outlook.com>
2025-02-09 23:16:59 +0800

    switched to bitset board

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char* argv[]) {
     const int screen_width = 800;
     const int screen_height = 600;
 
+    // reduces Moir�� pattern with higher-dimensional boards
     SetConfigFlags( FLAG_MSAA_4X_HINT );
 
     InitWindow( screen_width, screen_height, "Game of Life" );
@@ -20,19 +21,35 @@ int main(int argc, char* argv[]) {
 
     conway::set_up_camera();
 
+    // instance of cube initiated with zeroes
     conway::cube<conway::sim_array, conway::sim_mdspan> cube( conway::cell_states );
-    conway::board<conway::sim_array, conway::sim_mdspan> board( conway::cell_states );
 
+    // instance of toroidal cube initiated with zeroes
+    conway::toroidal_cube<conway::sim_array, conway::sim_mdspan, conway::board, conway::settings::cube_rank> t_cube( conway::cell_states );
+
+    // instance of board initiated with zeroes
+    conway::board_bitset<conway::sim_array, conway::sim_mdspan> board( conway::cell_states );
+
+    // set-up cube
     if ( strcmp( argv[ 1 ], "cube" ) == 0 ) {
         cube.seed_faces();
         cube.update_faces();
     }
 
+    // set-up toroidal cube
+    if ( strcmp( argv[ 1 ], "toroidal" ) == 0 ) {
+        t_cube.seed_cube();
+        t_cube.update_cube_padding();
+    }
+
+    // set-up board
     if ( strcmp( argv[ 1 ], "cylinder" ) == 0 || strcmp( argv[ 1 ], "board" ) == 0 ) {
         conway::seed( board );
         conway::update_padding( board );
     }
 
+    // blue cube = live cell
+    // white cube = dead cell
     Model blue_cube = LoadModelFromMesh( GenMeshCube( 0.25f, 0.25f, 0.25f ) );
     Model white_cube = LoadModelFromMesh( GenMeshCube( 0.25f, 0.25f, 0.25f ) );
 
@@ -47,6 +64,9 @@ int main(int argc, char* argv[]) {
         if ( strcmp( argv[ 1 ], "cylinder" ) == 0 )
             conway::draw_cylinder( board, blue_cube, white_cube );
 
+        if ( strcmp( argv[ 1 ], "toroidal" ) == 0 )
+            conway::draw_toroidal_cube( t_cube, blue_cube, white_cube );
+
         if ( strcmp( argv[ 1 ], "cube" ) == 0 )
             conway::draw_cube( cube, blue_cube, white_cube );
 
@@ -54,16 +74,27 @@ int main(int argc, char* argv[]) {
 
         EndDrawing();
 
+        // update cube faces
         if ( strcmp( argv[ 1 ], "cube" ) == 0 ) {
+            
             cube.evolve_faces();
             cube.update_faces();
         }
 
+        if ( strcmp( argv[ 1 ], "toroidal" ) == 0 ) {
+
+            t_cube.evolve_cube();
+            t_cube.update_cube_padding();
+        }
+
+        // update board
         if ( strcmp( argv[ 1 ], "cylinder" ) == 0 || strcmp( argv[ 1 ], "board" ) == 0 ) {
-            conway::evolve( board );
+
+            conway::evolve_bitset( board );
             conway::update_padding( board );
         }
 
+        // get user input
         conway::handle_controls();
 
     }
