ec8ca2bfb2ac1ffde578658f73b16eac5e008059
Nick <nicholashanson27@outlook.com>
2025-02-09 23:05:02 +0800

    modified blinker test

--- tests/test_blinker.cpp
+++ tests/test_blinker.cpp
@@ -1,10 +1,11 @@
 #include <boost/test/unit_test.hpp>
 #include "conway.hpp"
 
+using namespace conway;
+
 BOOST_AUTO_TEST_CASE( BlinkerTest )
 {
-    conway::board<conway::a49, conway::md7x7> bd( conway::blinker_period_0 );
-    conway::evolve( bd );
-    BOOST_CHECK( ( bd == conway::board<conway::a49, conway::md7x7>( conway::blinker_period_1 ) ) );
+    board<a49, md7x7> bd( blinker_period_0 );
+    evolve( bd );
+    BOOST_CHECK( ( bd == board<a49, md7x7>( blinker_period_1 ) ) );
 }
-
