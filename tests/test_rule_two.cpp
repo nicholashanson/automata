5c0e180ca49a964e020d60bbeb28b243c6227a0e
Nick <nicholashanson27@outlook.com>
2025-02-09 22:59:47 +0800

    modified rule two test

--- tests/test_rule_two.cpp
+++ tests/test_rule_two.cpp
@@ -1,6 +1,8 @@
 #include <boost/test/unit_test.hpp>
 #include "conway.hpp"
 
+using namespace conway;
+
 /*
     Rule two: Any live cell with two or three live neighbors lives
     on to the next generation.
@@ -9,15 +11,15 @@
 // cell survives with two live cells
 BOOST_AUTO_TEST_CASE( RuleTwo_2_Test )
 {
-    conway::conway_board_2d<conway::a25, conway::md5x5> bd( conway::rule_two_2_before );
-    conway::optimised_evolve( bd );
-    BOOST_CHECK( ( bd == conway::conway_board_2d<conway::a25, conway::md5x5>( conway::rule_two_2_after ) ) );
+    board<a25, md5x5> bd( rule_two_2_before );
+    evolve( bd );
+    BOOST_CHECK( ( bd == board<a25, md5x5>( rule_two_2_after ) ) );
 }
 
 // cell survives with three live cells
 BOOST_AUTO_TEST_CASE( RuleTwo_3_Test )
 {
-    conway::conway_board_2d<conway::a49, conway::md7x7> bd( conway::rule_two_3_before );
-    conway::optimised_evolve( bd );
-    BOOST_CHECK( ( bd == conway::conway_board_2d<conway::a49, conway::md7x7>( conway::rule_two_3_after ) ) );
+    board<a49, md7x7> bd( rule_two_3_before );
+    evolve( bd );
+    BOOST_CHECK( ( bd == board<a49, md7x7>( rule_two_3_after ) ) );
 }
